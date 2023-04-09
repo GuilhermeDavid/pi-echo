755713b4405fe67dc4ea319c80daffc1e675c01d
Guilherme David <guilhermedniza@gmail.com>
2023-04-09 17:04:10 -0300

    [09/04/2023] atualiza nome das classes e pacotes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:pi/entities/product.dart';
-import 'package:pi/screens/product_list_screen.dart';
+import 'package:pi/pages/product_list_screen.dart';
 
 void main() {
   runApp(MyApp());
