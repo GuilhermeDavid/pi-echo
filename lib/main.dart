3783b529049bb9565c2a571a2f21ea88a160122e
Guilherme David <guilhermedniza@gmail.com>
2023-04-12 15:37:21 -0300

    [12/04/2023] modifica a posicao da chamada da tela de produtos

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,19 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:pi/entities/product.dart';
 import 'package:pi/pages/product_list_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  
-  List<Product> _products = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchProducts();
-  }
-
-  Future<void> _fetchProducts() async {
-    final response = await http.get(Uri.parse('https://fakestoreapi.com/products'));
-    if (response.statusCode == 200) {
-      final jsonList = json.decode(response.body) as List<dynamic>;
-      final products = jsonList.map((json) => Product.fromJson(json)).toList();
-      setState(() {
-        _products = products;
-      });
-    }
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        body: _products.isEmpty ? 
-          Center(child: CircularProgressIndicator()) : 
-          ProductListScreen(products: _products),
+      title: 'Fake Store Products',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: ProductListScreen(),
     );
   }
 }
