30e07d32278f134cc429b4cd212f716b5147d0d4
Gabriel <gabrielmattoscosta@gmail.com>
2023-06-04 18:58:35 -0300

    add painel na tela de compra

--- lib/pages/Cart_screen.dart
+++ lib/pages/Cart_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:pi/entities/product.dart';
 import 'package:pi/entities/cart.dart';
@@ -15,56 +17,57 @@ class CartScreen extends StatelessWidget {
         backgroundColor: Color(0xFFE3762B),
       ),
       backgroundColor: Color(0xFFF2E2CE),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: cart.items.length,
-              itemBuilder: (context, index) {
-                final product = cart.items[index];
-                return ListTile(
-                  leading: Image.network(product.image),
-                  title: Text(product.title),
-                  subtitle: Text('Preço: \$${product.price}'),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () {
-                      cart.remove(product);
-                    },
-                  ),
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  'Total: \$${cart.total}',
-                  style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Poppins',
-                      color: Color(0xFFE3762B)),
-                ),
-                SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    cart.finalizarCompra(cart.items);
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              elevation: 4.0,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: cart.items.length,
+                  itemBuilder: (context, index) {
+                    final product = cart.items[index];
+                    return ListTile(
+                      leading: Image.network(product.image),
+                      title: Text(product.title),
+                      subtitle: Text('Preço: \$${product.price}'),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          cart.remove(product);
+                        },
+                      ),
+                    );
                   },
-                  style: ElevatedButton.styleFrom(
-                    primary:
-                        Color(0xFF38221f), // Define a cor de fundo do botão
-                  ),
-                  child: Text('Finalizar Compra'),
                 ),
-              ],
+              ),
             ),
-          ),
-          
-        ],
+            SizedBox(height: 16.0),
+            Text(
+              'Total: \$${cart.total}',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Poppins',
+                color: Color(0xFFE3762B),
+              ),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                cart.finalizarCompra(cart.items);
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFF38221f),
+              ),
+              child: Text('Finalizar Compra'),
+            ),
+          ],
+        ),
       ),
     );
   }
